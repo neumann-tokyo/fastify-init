ca80ca37e7c62ec0d29f627469ad9209179ace58
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-01-04 15:53:56 +0900

    feat: lint and format by biome

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,3 @@
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
+	fastify.get("/", async (request, reply) => ({ root: true }));
 }
--- test/plugins/support.test.js
+++ test/plugins/support.test.js
@@ -1,15 +1,15 @@
-import { test } from 'node:test'
-import * as assert from 'node:assert'
-import Fastify from 'fastify'
-import Support from '../../plugins/support.js'
+import * as assert from "node:assert";
+import { test } from "node:test";
+import Fastify from "fastify";
+import Support from "../../plugins/support.js";
 
-test('support works standalone', async (t) => {
-  const fastify = Fastify()
-  fastify.register(Support)
+test("support works standalone", async (t) => {
+	const fastify = Fastify();
+	fastify.register(Support);
 
-  await fastify.ready()
-  assert.equal(fastify.someSupport(), 'hugs')
-})
+	await fastify.ready();
+	assert.equal(fastify.someSupport(), "hugs");
+});
 
 // You can also use plugin with opts in fastify v2
 //
